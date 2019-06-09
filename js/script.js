50ce200a36b36696c332463cd7acbddd6aad94a0
Antonio <anton.valianos@gmail.com>
2019-06-09 16:46:47 +0300

    modal window

--- js/script.js
+++ js/script.js
@@ -34,4 +34,55 @@ window.addEventListener('DOMContentLoaded',function(){
         }
     });
 
+let deadline= '2019-06-14';
+
+function getTimeRemaining(endTime){
+    let t=Date.parse(endTime)-Date.parse(new Date()),
+    seconds=Math.floor((t/1000)%60),
+    minutes=Math.floor(((t/1000)/60)%60),
+    hours=Math.floor((t/(1000*60*60)));
+    return {
+        'total':t,
+        'hours':hours,
+        'minutes':minutes,
+        'seconds':seconds
+    };
+    //days=Math.floor((t/(1000*60*60*24)));
+}
+
+function setClock(id,endTime){
+    let timer = document.getElementById('timer'),
+    hours = timer.querySelector('.hours'),
+    minutes=timer.querySelector('.minutes'),
+    seconds=timer.querySelector('.seconds'),
+    timeInterval=setInterval(updateClock,1000); 
+    function updateClock(){
+        let t = getTimeRemaining(endTime);
+        hours.textContent=t.hours;
+        minutes.textContent=t.minutes;
+        seconds.textContent=t.seconds;
+        if(t.total<=0) {
+            clearInterval();
+        }
+    }
+}
+
+setClock('timer',deadline);
+
+//modal
+
+let more = document.querySelector('.more'),
+    overlay=document.querySelector('.overlay'),
+    close=document.querySelector('.popup-close');
+    more.addEventListener('click',function(){
+        overlay.style.display="block";
+        this.classList.add('more-splash');
+        document.body.style.overflow='hiden';
+    });
+
+    close.addEventListener('click',function(){
+        overlay.style.display="none";
+        more.classList.remove('more-splash');
+        document.body.style.overflow='';
+    });
 });
